3ad0bb3355d93f18be256ae060147db8ff8266aa
nathan-laing <ktmlaing@gmail.com>
2024-09-28 18:53:13 +1200

    feat: add url validator

---
+++ src/validators/url.validator.ts
@@ -0,0 +1,30 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+export function provideUrlValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+        const value = control.value;
+
+        if (!value) {
+            return null;
+        }
+
+        try {
+            new URL(value);
+        } catch {
+            return { invalidUrl: true };
+        }
+
+        /**
+         * Struggled to find a way to check for existence without a server to call
+         *
+         * Probably there is a way but from what I saw online they all looked very hacky
+         * And lots only worked for same origin URLs
+         */
+        const response = { status: 200 };
+        if (response.status === 404) {
+            return { nonExistentUrl: true };
+        }
+
+        return null;
+    };
+}
